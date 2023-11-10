8c59d5b45df3c2379cfc1702c1a71aeec789c93b
dpcks0509 <dpcks0509@gmail.com>
2023-11-10 18:15:03 +0900

    feat: 요청을 처리하는 클래스 추가
    요청을 처리하는 ChristmasController 클래스를 추가하였다.

---
+++ src/main/kotlin/christmas/controller/ChristmasController.kt
@@ -0,0 +1,13 @@
+package christmas.controller
+
+import christmas.view.InputView
+import christmas.view.OutputView
+
+class ChristmasController {
+    private val inputView = InputView()
+    private val outputView = OutputView()
+
+    fun run() {
+
+    }
+}
