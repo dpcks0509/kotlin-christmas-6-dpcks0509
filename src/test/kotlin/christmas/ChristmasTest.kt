f680e343871fb2f5d2a4ef7c26729abca183a4a9
dpcks0509 <dpcks0509@gmail.com>
2023-11-13 02:10:34 +0900

    test: 고객이 메뉴판에 있는 메뉴를 입력했는지 확인하는 테스트 추가
    고객이 메뉴판에 있는 메뉴를 입력했는지 확인하는 테스트를 추가하였다.

--- src/test/kotlin/christmas/ChristmasTest.kt
+++ src/test/kotlin/christmas/ChristmasTest.kt
@@ -1,5 +1,6 @@
 package christmas
 
+import christmas.util.Validator.validateOrders
 import christmas.util.Validator.validateVisitDay
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
@@ -17,5 +18,17 @@ class ChristmasTest {
     @ValueSource(strings = ["0", "32", "", " 1", "a"])
     fun `방문 날짜 1 이상 31 이하의 숫자 아닌 경우`(input: String) {
         assertThrows<IllegalArgumentException> { validateVisitDay(input) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["양송이수프-1", "티본스테이크-2", "초코케이크-3"])
+    fun `고객이 메뉴판에 있는 메뉴를 입력하는 경우ㅇ`(input: String) {
+        assertDoesNotThrow { validateOrders(input) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["옥수수수프-1", "함박스테이크-2", "딸기케이크-3"])
+    fun `고객이 메뉴판에 없는 메뉴를 입력하는 경우`(input: String) {
+        assertThrows<IllegalArgumentException> { validateOrders(input) }
     }
 }
