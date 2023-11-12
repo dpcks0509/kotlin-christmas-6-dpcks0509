03657f7dbf2691cb75915df2e04b0fb2a236775e
dpcks0509 <dpcks0509@gmail.com>
2023-11-12 23:54:44 +0900

    style: 필요없는 대괄호, else 제거
    필요없는 대괄호와 else를 제거하였다.

--- src/main/kotlin/christmas/view/OutputView.kt
+++ src/main/kotlin/christmas/view/OutputView.kt
@@ -10,58 +10,45 @@ class OutputView {
         private val decimalFormat = DecimalFormat("#,###")
     }
 
-    fun printErrorMessage(errorMessage: String) {
-        println(errorMessage)
-    }
+    fun printErrorMessage(errorMessage: String) = println(errorMessage)
 
-    fun printVisitDayInstruction() {
-        println(OutputMessage.VISIT_DAY_INSTRUCTION.getMessage())
-    }
+    fun printVisitDayInstruction() = println(OutputMessage.VISIT_DAY_INSTRUCTION.getMessage())
 
-    fun printOrderMenusInstruction() {
-        println(OutputMessage.ORDER_MENUS_INSTRUCTION.getMessage())
-    }
+    fun printOrderMenusInstruction() = println(OutputMessage.ORDER_MENUS_INSTRUCTION.getMessage())
 
-    fun printBenefitPreviewHeader(visitDay: Int) {
+    fun printBenefitPreviewHeader(visitDay: Int) =
         println(OutputMessage.BENEFIT_PREVIEW_HEADER.getMessage().format(visitDay))
-    }
 
-    fun printOrdersHeader() {
-        println(OutputMessage.ORDERS_HEADER.getMessage())
-    }
+    fun printOrdersHeader() = println(OutputMessage.ORDERS_HEADER.getMessage())
 
-    fun printOrders(orders: List<Order>) {
-        orders.forEach { order -> println(order) }
-    }
+    fun printOrders(orders: List<Order>) = orders.forEach { order -> println(order) }
 
-    fun printTotalOrderAmountBeforeDiscountHeader() {
+    fun printTotalOrderAmountBeforeDiscountHeader() =
         println(OutputMessage.TOTAL_ORDER_AMOUNT_BEFORE_DISCOUNT_HEADER.getMessage())
-    }
 
-    fun printTotalOrderAmountBeforeDiscount(totalOrderAmountBeforeDiscount: Int) {
+    fun printTotalOrderAmountBeforeDiscount(totalOrderAmountBeforeDiscount: Int) =
         println(OutputMessage.POSITIVE_AMOUNT.getMessage().format(decimalFormat.format(totalOrderAmountBeforeDiscount)))
-    }
 
-    fun printGiftMenuHeader() {
-        println(OutputMessage.GIFT_MENU_HEADER.getMessage())
-    }
 
-    fun printGiftMenu(gift: String) {
-        println(gift)
-    }
+    fun printGiftMenuHeader() = println(OutputMessage.GIFT_MENU_HEADER.getMessage())
+
+    fun printGiftMenu(gift: String) = println(gift)
+
 
     fun printBenefitDetailsHeader() {
         println(OutputMessage.BENEFIT_DETAILS_HEADER.getMessage())
     }
 
     fun printDDayDiscount(dDayDiscount: Int) {
-        if (dDayDiscount != 0)
-            println(OutputMessage.D_DAY_DISCOUNT.getMessage().format(decimalFormat.format(dDayDiscount)))
+        if (dDayDiscount != 0) println(
+            OutputMessage.D_DAY_DISCOUNT.getMessage().format(decimalFormat.format(dDayDiscount))
+        )
     }
 
     fun printWeekendDayDiscount(weekendDayDiscount: Int) {
-        if (weekendDayDiscount != 0)
-            println(OutputMessage.WEEKEND_DAY_DISCOUNT.getMessage().format(decimalFormat.format(weekendDayDiscount)))
+        if (weekendDayDiscount != 0) println(
+            OutputMessage.WEEKEND_DAY_DISCOUNT.getMessage().format(decimalFormat.format(weekendDayDiscount))
+        )
     }
 
     fun printWeekDayDiscount(weekDayDiscount: Int) {
@@ -70,44 +57,35 @@ class OutputView {
     }
 
     fun printSpecialDayDiscount(specialDayDiscount: Int) {
-        if (specialDayDiscount != 0)
-            println(OutputMessage.SPECIAL_DAY_DISCOUNT.getMessage().format(decimalFormat.format(specialDayDiscount)))
+        if (specialDayDiscount != 0) println(
+            OutputMessage.SPECIAL_DAY_DISCOUNT.getMessage().format(decimalFormat.format(specialDayDiscount))
+        )
     }
 
     fun printGiftBenefit(giftBenefit: Int) {
-        if (giftBenefit != 0)
-            println(OutputMessage.GIFT_BENEFIT.getMessage().format(decimalFormat.format(giftBenefit)))
+        if (giftBenefit != 0) println(OutputMessage.GIFT_BENEFIT.getMessage().format(decimalFormat.format(giftBenefit)))
     }
 
-    fun printNoBenefit() {
-        println(NO_BENEFIT)
-    }
+    fun printNoBenefit() = println(NO_BENEFIT)
 
-    fun printTotalBenefitAmountHeader() {
-        println(OutputMessage.TOTAL_BENEFIT_AMOUNT_HEADER.getMessage())
-    }
+    fun printTotalBenefitAmountHeader() = println(OutputMessage.TOTAL_BENEFIT_AMOUNT_HEADER.getMessage())
 
     fun printTotalBenefitAmount(totalBenefitAmount: Int) {
-        if (totalBenefitAmount != 0) {
-            println(OutputMessage.NEGATIVE_AMOUNT.getMessage().format(decimalFormat.format(totalBenefitAmount)))
-        } else {
-            println(OutputMessage.POSITIVE_AMOUNT.getMessage().format(decimalFormat.format(totalBenefitAmount)))
-        }
+        if (totalBenefitAmount == 0) println(
+            OutputMessage.POSITIVE_AMOUNT.getMessage().format(decimalFormat.format(totalBenefitAmount))
+        )
+        if (totalBenefitAmount != 0) println(
+            OutputMessage.NEGATIVE_AMOUNT.getMessage().format(decimalFormat.format(totalBenefitAmount))
+        )
     }
 
-    fun printTotalOrderAmountAfterDiscountHeader() {
+    fun printTotalOrderAmountAfterDiscountHeader() =
         println(OutputMessage.TOTAL_ORDER_AMOUNT_AFTER_DISCOUNT_HEADER.getMessage())
-    }
 
-    fun printTotalOrderAmountAfterDiscount(totalOrderAmountAfterDiscount: Int) {
+    fun printTotalOrderAmountAfterDiscount(totalOrderAmountAfterDiscount: Int) =
         println(OutputMessage.POSITIVE_AMOUNT.getMessage().format(decimalFormat.format(totalOrderAmountAfterDiscount)))
-    }
 
-    fun printBadgeHeader() {
-        println(OutputMessage.BADGE_HEADER.getMessage())
-    }
+    fun printBadgeHeader() = println(OutputMessage.BADGE_HEADER.getMessage())
 
-    fun printBadge(badge: String) {
-        println(badge)
-    }
+    fun printBadge(badge: String) = println(badge)
 }
