3223243ee1a9f07d042f45274b49aeaa6a54e988
dpcks0509 <dpcks0509@gmail.com>
2023-11-13 02:19:40 +0900

    test: 메뉴 형식이 예시와 다른 경우 테스트 추가
    메뉴 형식이 예시와 다른 경우 예외처리 확인하는 테스트를 추가하였다.
    중복되는 올바른 입력 확인 테스트를 삭제하였다.

--- src/test/kotlin/christmas/ChristmasTest.kt
+++ src/test/kotlin/christmas/ChristmasTest.kt
@@ -10,7 +10,7 @@ import org.junit.jupiter.params.provider.ValueSource
 class ChristmasTest {
     @ParameterizedTest
     @ValueSource(strings = ["1", "25", "31"])
-    fun `방문 날짜 1 이상 31 이하의 숫자인 경우`(input: String) {
+    fun `올바른 방문 날짜 입력`(input: String) {
         assertDoesNotThrow { validateVisitDay(input) }
     }
 
@@ -21,26 +21,20 @@ class ChristmasTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["양송이수프-1", "티본스테이크-2", "초코케이크-3"])
-    fun `고객이 메뉴판에 있는 메뉴를 입력하는 경우`(input: String) {
-        assertDoesNotThrow { validateOrders(input) }
-    }
-
-    @ParameterizedTest
     @ValueSource(strings = ["옥수수수프-1", "함박스테이크-2", "딸기케이크-3"])
     fun `고객이 메뉴판에 없는 메뉴를 입력하는 경우`(input: String) {
         assertThrows<IllegalArgumentException> { validateOrders(input) }
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["양송이수프-1", "티본스테이크-2", "초코케이크-3"])
-    fun `메뉴의 개수가 1 이상의 숫자인 경우`(input: String) {
-        assertDoesNotThrow { validateOrders(input) }
+    @ValueSource(strings = ["양송이수프-0", "티본스테이크--1", "초코케이크-one"])
+    fun `메뉴의 개수가 1 이상의 숫자가 아닌경우`(input: String) {
+        assertThrows<IllegalArgumentException> { validateOrders(input) }
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["양송이수프-0", "티본스테이크--1", "초코케이크-one"])
-    fun `메뉴의 개수가 1 이상의 숫자가 아닌경우`(input: String) {
+    @ValueSource(strings = ["양송이수프=1", "티본스테이크,2", "초코케이크- 3", "아이스크림 -4"])
+    fun `메뉴 형식이 예시와 다른 경우`(input: String) {
         assertThrows<IllegalArgumentException> { validateOrders(input) }
     }
 }
